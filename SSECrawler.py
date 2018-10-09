1d3d64448f166bfd4e469115bfaeb267e1518f21
Taylor Du <TaylorDu@duzhiruideMacBook-Pro.local>
2018-10-08 22:01:49 -0500

    1. Add Shenzhen Exchange inquiry letter crawler function, crawl_szse_inquiry_letters 2. Fix a bug of inquiry date

--- SSECrawler.py
+++ SSECrawler.py
@@ -4,6 +4,7 @@ import json
 from random import random
 import os.path
 from datetime import datetime
+import re
 def _download_pdf_by_url(url, filename='./temp.pdf'):
     response = requests.get(url)        
     if not os.path.isfile(filename):
@@ -22,7 +23,7 @@ def crawl_sse_inquiry_letters(start_page=1, end_page=1, is_download_letters=Fals
 
     columns_match_dict={'Ticker':'stockcode',
                         'Company_name':'extGSJC',
-                        'Date':'cmsOpDate',
+                        'Date':'createTime',#'cmsOpDate',
                         'Inquiry_type':'extWTFL',
                         'Title':'docTitle',
                         'Doc_url':'docURL'}
@@ -69,6 +70,87 @@ def crawl_sse_inquiry_letters(start_page=1, end_page=1, is_download_letters=Fals
                                                       row[0], 
                                                       row[1].replace('*',''),
                                                       row[4])
+                    if print_log:
+                        print('Start downloading PDF file: {}'.format(filename))
+                    _download_pdf_by_url(url,download_directory+filename)
+                except Exception as e:
+                    print(e)
+
+    return lines
+
+def crawl_szse_inquiry_letters(start_page=1, end_page=1, board=1, is_download_letters=False, download_directory='./', print_log=False, file_name='demo.csv'):
+    '''
+        board: 1:MB, 2:SME, 3:ChiNext
+        is_down_load_letters: choose if download all the inquiry letters pdf files
+    '''
+    headers_inquiry_letters = {'Host':"www.szse.cn",
+                               'Referer':"http://www.szse.cn/disclosure/supervision/inquire/index.html",
+                               'Accept':'*/*', 
+                               'UserAgent':'"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"'}
+    url_inquiry_letters = "http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=main_wxhj&TABKEY=tab{0}{1}&random={2}"
+
+    columns_match_dict={'Ticker':'gsdm',
+                        'Company_name':'gsjc',
+                        'Date':'fhrq',
+                        'Inquiry_type':'hjlb',
+                        'Title':'ck',
+                        'Doc_url':'ck'}
+    json_obj_num=board-1
+
+    lines=[]
+    line=''
+    for k in columns_match_dict.keys():
+        line+=k+','
+    lines.append(line.rstrip(','))
+    for page_num in range(start_page,end_page+1):
+        try:
+            if print_log:
+                print('Start crawling page {}'.format(page_num))
+            jscallback_patern=str(random())
+            if page_num==1:
+                url=url_inquiry_letters.format(board,"",jscallback_patern)
+            else:
+                url=url_inquiry_letters.format(board,"&PAGENO="+str(page_num),jscallback_patern)
+            response = requests.get(url=url,headers=headers_inquiry_letters)
+            json_object=json.loads(response.text)
+            row_count=len(json_object[json_obj_num]['data'])
+            
+            for i in range(row_count):
+                line=''
+                for k in columns_match_dict.keys():
+                    if k=='Title':
+                        start=json_object[json_obj_num]['data'][i]['ck'].find(r'>')
+                        end=json_object[json_obj_num]['data'][i]['ck'].find(r'</a>')
+                        line+=json_object[json_obj_num]['data'][i]['ck'][start+1:end]+','
+                    elif k=='Doc_url':
+                        start=json_object[json_obj_num]['data'][i]['ck'].find('fxklwxhj/')
+                        end=json_object[json_obj_num]['data'][i]['ck'].find(r"'>")
+                        line+=json_object[json_obj_num]['data'][i]['ck'][start+9:end]+','
+                    else:
+                        line+=json_object[json_obj_num]['data'][i][columns_match_dict[k]]+','
+                lines.append(line.rstrip(','))
+        except Exception as e:
+            print(e)
+
+    try:
+        with open(file_name,'w') as file:
+            for line in lines:
+                file.write(line)
+                file.write('\n')
+    except Exception as e:
+        print(e)
+
+    if len(lines)>1:
+        if is_download_letters:
+            for line in lines[1:]:
+                row=line.split(',')
+                try:
+                    jscallback_patern=str(random())
+                    url='http://reportdocs.static.szse.cn/UpFiles/fxklwxhj/{0}?random={1}'.format(row[5],jscallback_patern)
+                    filename='{}_{}_{}_{}'.format(datetime.strptime(row[2], '%Y-%m-%d').strftime("%Y%m%d"),
+                                                      row[0], 
+                                                      row[1].replace('*',''),
+                                                      row[5])
                     if print_log:
                         print('Start downloading PDF file: {}'.format(filename))
                     _download_pdf_by_url(url,download_directory+filename)
